a1d7086bb9937a3154f3d76623e757cf9ac30b4c
Khanhpv195 <khanhpoly@gmail.com>
2025-02-17 10:35:59 -0500

    Update

--- update-all.sh
+++ update-all.sh
@@ -3,6 +3,7 @@
 # Update API
 echo "Updating API..."
 cd api
+git reset --hard
 git pull
 sudo docker compose down -v
 sudo docker compose up -d
@@ -10,18 +11,21 @@ sudo docker compose up -d
 # Update Frontend
 echo "Updating Frontend..."
 cd ../fe
+git reset --hard
 git pull
 sudo docker compose up --build -d
 
 # Update Cleaner App
 echo "Updating Cleaner App..."
 cd ../app/cleaner
+git reset --hard
 git pull
 sudo docker compose restart
 
 # Update App
 echo "Updating App..."
 cd ../app/admin
+git reset --hard
 git pull
 sudo docker compose restart
 
