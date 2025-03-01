b6173c52e2a2124bac9579275a75d7d8025e3595
Khanhpv195 <khanhpoly@gmail.com>
2025-03-01 11:56:38 -0500

    Update

--- update-all.sh
+++ update-all.sh
@@ -14,19 +14,3 @@ cd ../fe
 git reset --hard
 git pull
 sudo docker compose up --build -d
-
-# Update Cleaner App
-echo "Updating Cleaner App..."
-cd ../app/cleaner
-git reset --hard
-git pull
-sudo docker compose restart
-
-# Update App
-echo "Updating App..."
-cd ../app/admin
-git reset --hard
-git pull
-sudo docker compose restart
-
-echo "All updates completed!" 
